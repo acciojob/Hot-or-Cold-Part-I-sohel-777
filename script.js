c9ecdce63b5d2013d641e4456e7b4af510a3fc7e
SK SOHEL RAHAMAN <117460184+sohel-777@users.noreply.github.com>
2023-02-12 14:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function guessNum() {
 }
 function print() {
 	if (Math.abs( randomNum - input) <= 5){
-		document.getElementById("respond").innerText= input;
+		document.getElementById("respond").innerText= "Hot";
 	}else{
-		document.getElementById("respond").innerHTML=  input;
+		document.getElementById("respond").innerHTML=  "Cold";
 		
 	}
 }
