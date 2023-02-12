6629769ebca250fbe37384fca8603073b9350239
SK SOHEL RAHAMAN <117460184+sohel-777@users.noreply.github.com>
2023-02-12 14:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+var randomNum;
+var input;
 
+function randomNumGenerator() {
+	randomNum = Math.floor(Math.random() * 41) - 20;
+	document.getElementById("num").innerText= randomNum;
+	guessNum();
+	print()
+	
+}
+
+function guessNum() {
+	 
+	input=Number(document.getElementById("guess").value);
+	 
+
+}
+function print() {
+	if (Math.abs( randomNum - input) <= 5){
+		document.getElementById("respond").innerText= input;
+	}else{
+		document.getElementById("respond").innerHTML=  input;
+		
+	}
+}
